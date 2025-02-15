628eccb0f9f2b0d4ad4df49c02d300edaf901dca
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-02-14 21:25:57 -0500

    reforge clientside hotfix

--- src/main/java/io/github/racoondog/bsmpcompanion/mixin/reforge/client/TieredClientPacketMixin.java
+++ src/main/java/io/github/racoondog/bsmpcompanion/mixin/reforge/client/TieredClientPacketMixin.java
@@ -3,8 +3,11 @@ package io.github.racoondog.bsmpcompanion.mixin.reforge.client;
 import draylar.tiered.network.TieredClientPacket;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Environment(EnvType.CLIENT)
 @Mixin(value = TieredClientPacket.class, remap = false)
@@ -16,5 +19,14 @@ public class TieredClientPacketMixin {
     @Overwrite
     public static void writeC2SSyncPosPacket(Boolean reforgeHandler) {
         // do nothing
+    }
+
+    /**
+     * Prevent crash by writing a dummy value instead of a null value.
+     * @author Crosby
+     */
+    @ModifyArg(method = "writeC2SScreenPacket", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/PacketByteBuf;writeBlockPos(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/network/PacketByteBuf;", remap = true))
+    private static BlockPos preventCrash(BlockPos pos) {
+        return BlockPos.ORIGIN; // use dummy value
     }
 }
